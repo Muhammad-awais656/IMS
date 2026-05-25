9a37aba6353266635e0c1072c25042129feee5bc
Muhammad-awais656 <awais.uet656@gmail.com>
2026-05-25 14:48:19 +0500

    updated

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -17,8 +17,7 @@ namespace IMS.Models
         /// <summary>
         /// Kept for connection string selection (e.g. Shop/Factory). Set server-side on login.
         /// </summary>
-        [ValidateNever]
-        public string Domain { get; set; } = "Shop";
+   
 
         [ValidateNever]
        public DAL.PrimaryDBContext.User ShopUsers { get; set; }
